3088855ccd2655fb2bb1f8689972c7f93a8ceaa3
Mohd Sufyan khan <sufyankhan.01127@gmail.com>
2026-07-27 02:12:59 +0530

    Added Python new programs

---
+++ p4.py
@@ -0,0 +1,26 @@
+# all data types in python are objects. In python, we can perform arithmetic operations on the input values as they are treated as strings. We can also perform arithmetic operations on the input values after converting them into integer or float.
+
+a="ahks"
+b=10
+c=10.5
+d=True
+e='a'
+f={2,1,4}
+g=[1,2,3]
+
+h=None
+i={"name":"arhan","age":10}
+j={"arhan","ahks","ahks"}
+k=(1,"arfat",4.5)
+
+print(type(a),a)
+print(type(b),b)
+print(type(c),c)
+print(type(d),d)
+print(type(e),e)
+print(type(f),f)
+print(type(g),g)
+print(type(h),h)
+print(type(i),i)
+print(type(j),j)
+print(type(k),k)
