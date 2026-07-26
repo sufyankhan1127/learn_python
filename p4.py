90517e9bc70a28f224f6d3767d1394acfdceaba4
Mohd Sufyan khan <sufyankhan.01127@gmail.com>
2026-07-27 02:32:29 +0530

    Added Python new programs

--- p4.py
+++ p4.py
@@ -1,17 +1,22 @@
 # all data types in python are objects. In python, we can perform arithmetic operations on the input values as they are treated as strings. We can also perform arithmetic operations on the input values after converting them into integer or float.
+# data types in python are:
 
-a="ahks"
-b=10
-c=10.5
-d=True
-e='a'
-f={2,1,4}
-g=[1,2,3]
 
-h=None
-i={"name":"arhan","age":10}
-j={"arhan","ahks","ahks"}
-k=(1,"arfat",4.5)
+
+
+a="ahks" #PD->Sequence data type->Strings
+b=10 #PD->Numeric data type->Integer
+c=10.5 #PD->Numeric data type->Float
+d=True #PD->Boolean data type
+l=4+5j #PD->Numeric data type->Complex
+e='a' #PD->Sequence data type->String
+f={2,1,4} #PD->Set data type
+g=[1,2,3] #PD->Sequence data type->List
+
+h=None #PD->None data type
+i={"name":"arhan","age":10} #PD->Mapping data type->Dictionary
+j={"arhan","ahks","ahks"} #PD->Set data type
+k=(1,"arfat",4.5) #PD->Sequence data type->Tuple
 
 print(type(a),a)
 print(type(b),b)
@@ -24,3 +29,8 @@ print(type(h),h)
 print(type(i),i)
 print(type(j),j)
 print(type(k),k)
+print(type(l),l)
+
+
+complex_num=complex(4)
+print("Complex number is",complex_num)
