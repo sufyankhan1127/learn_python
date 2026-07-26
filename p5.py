68660ca827d355bc91c92196fd1eeca151e9b6d4
Mohd Sufyan khan <sufyankhan.01127@gmail.com>
2026-07-27 02:35:10 +0530

    Added swapping of numbers

---
+++ p5.py
@@ -0,0 +1,17 @@
+# swapping of two numbers
+
+a=10
+b=20
+
+# temp=a
+# a=b
+# b=temp
+# print("After swapping a is",a)
+# print("After swapping b is",b)
+
+
+#swapping of two numbers without using third variable
+
+a,b=b,a
+print("After swapping a is",a)
+print("After swapping b is",b)
